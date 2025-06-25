2deee8245bc0b6743d4aad64541357c48efc7240
Nox Fox <rickxxxmorty@proton.me>
2025-06-26 02:05:32 +0300

    Update UploadService.js

--- src/UploadService.js
+++ src/UploadService.js
@@ -710,11 +710,6 @@ const UploadService = () => {
 
 
   </div>
-); => clearTimeout(timer); // Cleanup timer
-  }, []);
-   const handleCheckboxChange = (event) => {
-    setIsdelChecked(event.target.checked);
-  };
   // Loader component
   const Loader = () => (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
